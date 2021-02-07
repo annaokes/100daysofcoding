a9c3b831cc2eba3617b0032ae9eecb7d9eeae6c7
annaokes <44604312+annaokes@users.noreply.github.com>
2021-02-07 15:30:04 +0000

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,25 @@
+# Day 1 of 100days of coding project: Band Name Generator
+
+# Welcoming user to the band name generator
+
+print("Welcome to Band Name Generator " + input("What is your name? "))
+
+# Ask the user for the city they grew up in
+
+city_name = input("What city did you grow up in? ")
+
+# Ask the user what their pet name is or what their favourite animal is ?
+
+pet_name = input("What is your pets name? Or if you don't have one, what is your favourite animal? ")
+
+# Ask for a random colour
+
+random_colour = input("Please give me a random colour: ")
+
+print("Your new band name is... drum roll please")
+print(city_name + pet_name + random_colour)
+print("Hope you like it :)")
+
+
+
+
